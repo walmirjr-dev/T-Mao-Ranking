4049311bfcb3c10ac47f0373b0f98322242b5056
Walmir Alves <walmiralves.dev@gmail.com>
2026-06-01 15:26:42 -0300

    Initial commit

---
+++ tmaoranking/src/main/java/com/walmir/tmaoranking/TmaorankingApplication.java
@@ -0,0 +1,13 @@
+package com.walmir.tmaoranking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmaorankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmaorankingApplication.class, args);
+	}
+
+}
---
+++ tmaoranking/src/test/java/com/walmir/tmaoranking/TmaorankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walmir.tmaoranking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmaorankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
