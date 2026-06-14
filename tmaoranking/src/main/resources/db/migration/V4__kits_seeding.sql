c62587be59ed807f78477c3116867292f7953949
Walmir Alves <walmiralves.dev@gmail.com>
2026-06-14 18:55:44 -0300

    Kits seeding

---
+++ tmaoranking/src/main/resources/db/migration/V4__kits_seeding.sql
@@ -0,0 +1,36 @@
+-- Migration V4: População inicial de Camisas (2026 a 2020) com links do GitHub
+
+INSERT INTO kits (kit_year, name, kit_type, image_url, description) VALUES
+-- 2026
+(2026, 'Camisa 1 2026', 'HOME', 'https://raw.githubusercontent.com/walmirjr-dev/tmao-assets/main/camisa1_2026.webp', NULL),
+(2026, 'Camisa 2 2026', 'AWAY', 'https://raw.githubusercontent.com/walmirjr-dev/tmao-assets/main/camisa2_2026.webp', NULL),
+
+-- 2025
+(2025, 'Camisa 1 2025', 'HOME', 'https://raw.githubusercontent.com/walmirjr-dev/tmao-assets/main/camisa1_2025.webp', NULL),
+(2025, 'Camisa 2 2025', 'AWAY', 'https://raw.githubusercontent.com/walmirjr-dev/tmao-assets/main/camisa2_2025.webp', NULL),
+(2025, 'Camisa 3 2025', 'SPECIAL', 'https://raw.githubusercontent.com/walmirjr-dev/tmao-assets/main/camisa3_2025.webp', NULL),
+
+-- 2024
+(2024, 'Camisa 1 2024', 'HOME', 'https://raw.githubusercontent.com/walmirjr-dev/tmao-assets/main/camisa1_2024.webp', NULL),
+(2024, 'Camisa 2 2024', 'AWAY', 'https://raw.githubusercontent.com/walmirjr-dev/tmao-assets/main/camisa2_2024.webp', NULL),
+(2024, 'Camisa 3 2024', 'SPECIAL', 'https://raw.githubusercontent.com/walmirjr-dev/tmao-assets/main/camisa3_2024.webp', NULL),
+
+-- 2023
+(2023, 'Camisa 1 2023', 'HOME', 'https://raw.githubusercontent.com/walmirjr-dev/tmao-assets/main/camisa1_2023.webp', NULL),
+(2023, 'Camisa 2 2023', 'AWAY', 'https://raw.githubusercontent.com/walmirjr-dev/tmao-assets/main/camisa2_2023.webp', NULL),
+(2023, 'Camisa 3 2023', 'SPECIAL', 'https://raw.githubusercontent.com/walmirjr-dev/tmao-assets/main/camisa3_2023.webp', NULL),
+
+-- 2022
+(2022, 'Camisa 1 2022', 'HOME', 'https://raw.githubusercontent.com/walmirjr-dev/tmao-assets/main/camisa1_2022.webp', NULL),
+(2022, 'Camisa 2 2022', 'AWAY', 'https://raw.githubusercontent.com/walmirjr-dev/tmao-assets/main/camisa2_2022.webp', NULL),
+(2022, 'Camisa 3 2022', 'SPECIAL', 'https://raw.githubusercontent.com/walmirjr-dev/tmao-assets/main/camisa3_2022.webp', NULL),
+
+-- 2021
+(2021, 'Camisa 1 2021', 'HOME', 'https://raw.githubusercontent.com/walmirjr-dev/tmao-assets/main/camisa1_2021.webp', NULL),
+(2021, 'Camisa 2 2021', 'AWAY', 'https://raw.githubusercontent.com/walmirjr-dev/tmao-assets/main/camisa2_2021.webp', NULL),
+(2021, 'Camisa 3 2021', 'SPECIAL', 'https://raw.githubusercontent.com/walmirjr-dev/tmao-assets/main/camisa3_2021.webp', NULL),
+
+-- 2020
+(2020, 'Camisa 1 2020', 'HOME', 'https://raw.githubusercontent.com/walmirjr-dev/tmao-assets/main/camisa1_2020.webp', NULL),
+(2020, 'Camisa 2 2020', 'AWAY', 'https://raw.githubusercontent.com/walmirjr-dev/tmao-assets/main/camisa2_2020.webp', NULL),
+(2020, 'Camisa 3 2020', 'SPECIAL', 'https://raw.githubusercontent.com/walmirjr-dev/tmao-assets/main/camisa3_2020.webp', NULL);
