745905bb53b33495a491bced54fe8f7e97e60fcf
Walmir Alves <walmiralves.dev@gmail.com>
2026-06-14 18:58:41 -0300

    fix: fix bug where public routes were being intercepted by JwtFilter

--- tmaoranking/src/main/java/com/walmir/tmaoranking/security/JwtFilter.java
+++ tmaoranking/src/main/java/com/walmir/tmaoranking/security/JwtFilter.java
@@ -28,6 +28,15 @@ public class JwtFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
+
+        String path = request.getRequestURI();
+
+        // ignora rotas públicas
+        if (path.equals("/auth/login") || path.equals("/users") && request.getMethod().equals("POST")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
         String authHeader = request.getHeader("Authorization");
 
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
